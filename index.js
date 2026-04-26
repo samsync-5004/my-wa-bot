1c49b6a1ce496e141812df1495861af505c7ac62
samsync-5004 <samuelolami5004@gmail.com>
2026-04-26 14:42:13 +0100

    fs update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const geminiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
